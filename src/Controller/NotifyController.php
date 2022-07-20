f1f9b32d3bdbf5e7115c4645bb02c13f741e3d9a
Ahmed Mohamed 04 <eng.a7medfci@gmail.com>
2022-07-20 09:39:52 +0200

    send logs to slack

--- src/Controller/NotifyController.php
+++ src/Controller/NotifyController.php
@@ -11,7 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Payum\Core\Payum;
 use Sylius\Component\Core\Model\PaymentInterface;
-
+use Monolog\Level;
+use Monolog\Logger;
 class NotifyController extends AbstractController
 {
     /** @var Payum */
@@ -20,68 +21,89 @@ class NotifyController extends AbstractController
     /** @var PaymobServiceInterface */
     private $paymobService;
 
+    /**
+     * @var Logger 
+     */
+    private $log;
+
     public function __construct(
         Payum $payum,
-        PaymobServiceInterface $paymobService
+        PaymobServiceInterface $paymobService,
+        Logger  $log
     ) {
         $this->payum = $payum;
         $this->paymobService = $paymobService;
+        $this->log = $log;
+
     }
 
     public function doAction(Request $request): Response
     {
-        $_GET_PARAMS = $request->query->all();
+        try {
+            $_GET_PARAMS = $request->query->all();
 
-        if(!empty($_GET_PARAMS) && $_GET_PARAMS['success'] == 'true') {
-            return $this->redirectToRoute('sylius_shop_order_thank_you');
+            if(!empty($_GET_PARAMS) && $_GET_PARAMS['success'] == 'true') {
+                return $this->redirectToRoute('sylius_shop_order_thank_you');
+            }
+
+            $order = $this->paymobService->getPaymentById($_GET_PARAMS['merchant_order_id'])->getOrder();
+        }
+        catch(\Exception $ex){
+            $this->log->emergency($ex);
         }
 
-        $order = $this->paymobService->getPaymentById($_GET_PARAMS['merchant_order_id'])->getOrder();
         return $this->redirectToRoute('sylius_shop_order_show',['tokenValue' => $order->getTokenValue()]);
     }
 
     public function webhookAction(Request $request): Response
     {
-        $paymobResponse = \GuzzleHttp\json_decode($request->getContent());
-        $response = false;
 
-        //success payment
-        if(
-            !empty($paymobResponse) &&
-            isset($paymobResponse->obj->is_standalone_payment) &&
-            isset($paymobResponse->obj->success) && $paymobResponse->obj->success &&
-            isset($paymobResponse->type) && $paymobResponse->type == PaymobService::TRANSACTION_TYPE &&
-            isset($paymobResponse->obj->order->paid_amount_cents) &&
-            isset($paymobResponse->obj->order->merchant_order_id)
-        ) {
-            $payment = $this->paymobService->getPaymentById($paymobResponse->obj->order->merchant_order_id);
-
-            $orderAmount = $paymobResponse->obj->order->paid_amount_cents;
-            $amount = $payment->getAmount();
-
-            if($orderAmount === $amount) {
-                $payment->setDetails(['status'=> 'success', 'message' => "amount: {$amount}"]);
+        try{
+            $paymobResponse = \GuzzleHttp\json_decode($request->getContent());
+            $response = false;
+    
+            //success payment
+            if(
+                !empty($paymobResponse) &&
+                isset($paymobResponse->obj->is_standalone_payment) &&
+                isset($paymobResponse->obj->success) && $paymobResponse->obj->success &&
+                isset($paymobResponse->type) && $paymobResponse->type == PaymobService::TRANSACTION_TYPE &&
+                isset($paymobResponse->obj->order->paid_amount_cents) &&
+                isset($paymobResponse->obj->order->merchant_order_id)
+            ) {
+                $payment = $this->paymobService->getPaymentById($paymobResponse->obj->order->merchant_order_id);
+    
+                $orderAmount = $paymobResponse->obj->order->paid_amount_cents;
+                $amount = $payment->getAmount();
+    
+                if($orderAmount === $amount) {
+                    $payment->setDetails(['status'=> 'success', 'message' => "amount: {$amount}"]);
+                    $order = $this->paymobService->setPaymentState($payment,
+                        PaymentInterface::STATE_COMPLETED,
+                        OrderPaymentStates::STATE_PAID
+                    );
+                    $response = true;
+                }
+            } else if (isset($paymobResponse->obj->order->merchant_order_id)) {
+                $paymentId = $paymobResponse->obj->order->merchant_order_id;
+                $payment = $this->paymobService->getPaymentById($paymentId);
+                $payment->setDetails(["status"=> "failed", "message"=> "payment_id: {$paymentId}"]);
+    
+                # create new payment so user can try to pay again
+                $newPayment = clone $payment;
+                $newPayment->setState(PaymentInterface::STATE_NEW);
+                $payment->getOrder()->addPayment($newPayment);
+    
                 $order = $this->paymobService->setPaymentState($payment,
-                    PaymentInterface::STATE_COMPLETED,
-                    OrderPaymentStates::STATE_PAID
+                    PaymentInterface::STATE_FAILED,
+                    OrderPaymentStates::STATE_AWAITING_PAYMENT
                 );
-                $response = true;
             }
-        } else if (isset($paymobResponse->obj->order->merchant_order_id)) {
-            $paymentId = $paymobResponse->obj->order->merchant_order_id;
-            $payment = $this->paymobService->getPaymentById($paymentId);
-            $payment->setDetails(["status"=> "failed", "message"=> "payment_id: {$paymentId}"]);
-
-            # create new payment so user can try to pay again
-            $newPayment = clone $payment;
-            $newPayment->setState(PaymentInterface::STATE_NEW);
-            $payment->getOrder()->addPayment($newPayment);
-
-            $order = $this->paymobService->setPaymentState($payment,
-                PaymentInterface::STATE_FAILED,
-                OrderPaymentStates::STATE_AWAITING_PAYMENT
-            );
         }
+        catch(\Exception $ex){
+            $this->log->emergency($ex);
+        }
+        
 
         return new Response(\GuzzleHttp\json_encode(['success' => $response]), $response ? 200 : 400);
     }
