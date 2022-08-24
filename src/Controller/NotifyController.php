0a1f823f3b2079f0f0d30e399978a84d13ffcb50
Ahmed Mohamed 04 <eng.a7medfci@gmail.com>
2022-08-24 14:24:23 +0200

    follow psr2 :)

--- src/Controller/NotifyController.php
+++ src/Controller/NotifyController.php
@@ -13,6 +13,7 @@ use Payum\Core\Payum;
 use Sylius\Component\Core\Model\PaymentInterface;
 use Monolog\Level;
 use Monolog\Logger;
+
 class NotifyController extends AbstractController
 {
     /** @var Payum */
@@ -22,19 +23,19 @@ class NotifyController extends AbstractController
     private $paymobService;
 
     /**
-     * @var Logger 
+     * @var Logger
      */
     private $log;
 
     public function __construct(
-        Payum $payum,
+        Payum                  $payum,
         PaymobServiceInterface $paymobService,
-        Logger  $log
-    ) {
+        Logger                 $log
+    )
+    {
         $this->payum = $payum;
         $this->paymobService = $paymobService;
         $this->log = $log;
-
     }
 
     public function doAction(Request $request): Response
@@ -46,31 +47,25 @@ class NotifyController extends AbstractController
 
             if (!empty($_GET_PARAMS) && $_GET_PARAMS['success'] == 'true') {
 
-                if ($order->getChannel()->getCode() == '3attar_web')
+                if ($order->getChannel()->getCode() == '3attar_web') {
                     return $this->redirectToRoute('sylius_shop_order_thank_you');
-                else
+                } else {
                     return $this->redirect('https://3attar.page.link?apn=com.attar.app&ibi=com.3attar.ios.app&link=https://3attar.com?payment=1');
+                }
             }
-        }
-        catch(\Exception $ex){
+        } catch (\Exception $ex) {
             $this->log->emergency($ex);
         }
 
-        return $this->redirectToRoute('sylius_shop_order_show',['tokenValue' => $order->getTokenValue()]);
+        return $this->redirectToRoute('sylius_shop_order_show', ['tokenValue' => $order->getTokenValue()]);
     }
 
     public function webhookAction(Request $request): Response
     {
-
-        try{
-
-           
-
+        try {
             $paymobResponse = \GuzzleHttp\json_decode($request->getContent());
             $response = false;
-    
-            //success payment
-            if(
+            if (
                 !empty($paymobResponse) &&
                 isset($paymobResponse->obj->is_standalone_payment) &&
                 isset($paymobResponse->obj->success) && $paymobResponse->obj->success &&
@@ -79,12 +74,12 @@ class NotifyController extends AbstractController
                 isset($paymobResponse->obj->order->merchant_order_id)
             ) {
                 $payment = $this->paymobService->getPaymentById($paymobResponse->obj->order->merchant_order_id);
-    
+
                 $orderAmount = $paymobResponse->obj->order->paid_amount_cents;
                 $amount = $payment->getAmount();
-    
-                if($orderAmount === $amount) {
-                    $payment->setDetails(['status'=> 'success', 'message' => "amount: {$amount}"]);
+
+                if ($orderAmount === $amount) {
+                    $payment->setDetails(['status' => 'success', 'message' => "amount: {$amount}"]);
                     $order = $this->paymobService->setPaymentState($payment,
                         PaymentInterface::STATE_COMPLETED,
                         OrderPaymentStates::STATE_PAID
@@ -94,13 +89,13 @@ class NotifyController extends AbstractController
             } else if (isset($paymobResponse->obj->order->merchant_order_id)) {
                 $paymentId = $paymobResponse->obj->order->merchant_order_id;
                 $payment = $this->paymobService->getPaymentById($paymentId);
-                $payment->setDetails(["status"=> "failed", "message"=> "payment_id: {$paymentId}"]);
-    
+                $payment->setDetails(["status" => "failed", "message" => "payment_id: {$paymentId}"]);
+
                 # create new payment so user can try to pay again
                 $newPayment = clone $payment;
                 $newPayment->setState(PaymentInterface::STATE_NEW);
                 $payment->getOrder()->addPayment($newPayment);
-    
+
                 $order = $this->paymobService->setPaymentState($payment,
                     PaymentInterface::STATE_FAILED,
                     OrderPaymentStates::STATE_AWAITING_PAYMENT
@@ -108,14 +103,11 @@ class NotifyController extends AbstractController
             }
             $this->log->emergency("paymob callback");
             $this->log->emergency($request);
-            
-        }
-        catch(\Exception $ex){
+
+        } catch (\Exception $ex) {
             $this->log->emergency($request);
             $this->log->emergency($ex);
         }
-        
-
         return new Response(\GuzzleHttp\json_encode(['success' => $response]), $response ? 200 : 400);
     }
 }
