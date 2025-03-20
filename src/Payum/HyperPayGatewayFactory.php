868866e3bf51a8e31cad4a290e68d3aacebcf74d
Ahmed Mohamed <eng.a7medfci@gmail.com>
2025-03-20 08:13:53 +0200

    create form for hyperpay gateway , create payum api to match sylius requirement , and register it in services

---
+++ src/Payum/HyperPayGatewayFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ahmedkhd\SyliusPaymobPlugin\Payum;
+
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\GatewayFactory;
+use Sylius\PayPalPlugin\Payum\Action\StatusAction;
+
+final class HyperPayGatewayFactory extends GatewayFactory
+{
+    protected function populateConfig(ArrayObject $config): void
+    {
+        $config->defaults([
+            'payum.factory_name' => 'hyperpay',
+            'payum.factory_title' => 'Hyperpay',
+            'payum.action.status' => new StatusAction(),
+        ]);
+
+        $config['payum.api'] = function (ArrayObject $config) {
+            return new SyliusApiHyperPay(
+                $config['authToken'],
+                $config['entityId'],
+                $config['iframe_url'],
+                $config['domain'],
+            );
+        };
+    }
+}
