57891c3d179a607db95547dc3b5dec6c08380b1e
Soliman Abaza <soliman1samy@gmail.com>
2025-04-19 06:24:46 +0200

    Inject logger

--- src/Services/TamaraService.php
+++ src/Services/TamaraService.php
@@ -5,19 +5,22 @@ namespace App\Service;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request as GuzzleRequest;
+use Psr\Log\LoggerInterface;
 
 class TamaraService
 {
     private $client;
     private $tamaraToken;
     private $baseUrl;
+    private $logger;
 
     const SANDBOX_BASE_URL = 'https://api-sandbox.tamara.co';
     const LIVE_BASE_URL = 'https://api.tamara.co';
 
-    public function __construct(Client $client, string $tamaraToken, string $tamaraEnv)
+    public function __construct(Client $client, LoggerInterface $logger, string $tamaraToken, string $tamaraEnv)
     {
         $this->client = $client;
+        $this->logger = $logger;
         $this->tamaraToken = $tamaraToken;
         $this->baseUrl = $tamaraEnv === 'sandbox' ? self::SANDBOX_BASE_URL : self::LIVE_BASE_URL;
     }
@@ -35,10 +38,25 @@ class TamaraService
             );
 
             $response = $this->client->send($request);
-            $body = $response->getBody()->getContents();
 
-            return json_decode($body, true);
+            // Check if the response status is 200 (OK)
+            if ($response->getStatusCode() === 200) {
+                $body = $response->getBody()->getContents();
+                return json_decode($body, true);
+            }
+
+            // Log response status and body if not 200
+            $this->logger->error("Tamara API Error", [
+                'status_code' => $response->getStatusCode(),
+                'body' => $response->getBody()->getContents()
+            ]);
+
+            return null;
         } catch (\Exception $e) {
+            // Log the exception
+            $this->logger->error("Error fetching Tamara order", [
+                'exception' => $e->getMessage()
+            ]);
             return null;
         }
     }
